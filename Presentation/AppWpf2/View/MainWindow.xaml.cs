1e3c03005298f16127dc369963ca3999e4039b1b
Francesco Crimi <francrim@gmail.com>
2019-11-01 02:52:05 +0100

    Rename AppWpf2

---
+++ Presentation/AppWpf2/View/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using CiccioGest.Presentation.AppWpf2.ViewModel;
+using GalaSoft.MvvmLight.Messaging;
+using System.Windows;
+
+namespace CiccioGest.Presentation.AppWpf2.View
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            RegistraMessaggi();
+            Closing += MainWindow_Closing;
+        }
+
+        private void RegistraMessaggi()
+        {
+            Messenger.Default.Register<NotificationMessage>(this, ns =>
+            {
+                if (ns.Notification == "ApriFatturaView")
+                {
+                     new FatturaView().Show();
+                }
+                else if (ns.Notification == "ApriSelezionaFatturaView")
+                {
+                    new SelezionaFatturaView().ShowDialog();
+                }
+                else if (ns.Notification == "ApriProdotti")
+                {
+                     new ProdottoView().Show();
+                }
+                else if (ns.Notification == "ApriCategorie")
+                {
+                     new CategoriaView().Show();
+                }
+            });
+        }
+
+        private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            App.Windsor.Release(DataContext);
+            Messenger.Default.Unregister(this);
+            ViewModelLocator.Cleanup();
+        }
+    }
+}
