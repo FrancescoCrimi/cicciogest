09e841e44c45acc7e2253134265ecafea9cae24c
Francesco Crimi <francrim@gmail.com>
2019-12-31 05:07:31 +0100

    uwp wcf config fix

--- Presentation/Uwp/Client/Wcf/FatturaService.cs
+++ Presentation/Uwp/Client/Wcf/FatturaService.cs
@@ -1,4 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using CiccioGest.Domain.ClientiFornitori;
+using CiccioGest.Domain.Documenti;
+using CiccioGest.Domain.Magazino;
+using CiccioGest.Infrastructure.Conf;
+using System.Collections.Generic;
 using System.ServiceModel.Description;
 using System.Threading.Tasks;
 
@@ -29,36 +33,46 @@ namespace CiccioGest.Presentation.Uwp.Client.Wcf
 
     internal class FatturaService : FatturaServiceClient, Application.IFatturaService
     {
+        public FatturaService(IConf conf)
+            : base(EndpointConfiguration.BasicHttpBinding_IFatturaService, GetRemoteAddress(conf))
+        {
+        }
+
+        private static string GetRemoteAddress(IConf conf)
+        {
+            return conf.CS + "/fatturaservice.svc";
+        }
+
         public Task DeleteFattura(int id)
         {
             return DeleteFatturaAsync(id);
         }
 
-        public Task<Domain.Magazino.Articolo> GetArticolo(int id)
+        public Task<Articolo> GetArticolo(int id)
         {
             return GetArticoloAsync(id);
         }
 
-        public Task<Domain.ClientiFornitori.Cliente> GetCliente(int idCliente)
+        public Task<Cliente> GetCliente(int idCliente)
         {
             return GetClienteAsync(idCliente);
         }
 
-        public Task<Domain.Documenti.Fattura> GetFattura(int id)
+        public Task<Fattura> GetFattura(int id)
         {
             return GetFatturaAsync(id);
         }
 
-        public Task<IList<Domain.Documenti.FatturaReadOnly>> GetFatture()
+        public Task<IList<FatturaReadOnly>> GetFatture()
         {
             return Task.Run(async () =>
             {
                 var lst = await GetFattureAsync();
-                return (IList<Domain.Documenti.FatturaReadOnly>)lst;
+                return (IList<FatturaReadOnly>)lst;
             });
         }
 
-        public Task<Domain.Documenti.Fattura> SaveFattura(Domain.Documenti.Fattura fattura)
+        public Task<Fattura> SaveFattura(Fattura fattura)
         {
             return SaveFatturaAsync(fattura);
         }
