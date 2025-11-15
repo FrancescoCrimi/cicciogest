2482c93b5639382ce7041af6c6f325687a8393b3
Francesco Crimi <francrim@hotmail.com>
2025-11-15 02:25:21 +0100

    add sample  window, remove windows 7 from wpf project

---
+++ Presentation/Wpf/WpfApp/Test/ArticoloView3.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CiccioGest.Presentation.WpfApp.Test
+{
+    /// <summary>
+    /// Logica di interazione per ArticoloView3.xaml
+    /// </summary>
+    public partial class ArticoloView3 : Window
+    {
+        public ArticoloView3()
+        {
+            InitializeComponent();
+        }
+    }
+}
